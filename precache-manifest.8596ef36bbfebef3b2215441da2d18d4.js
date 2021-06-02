966204f6dd22d1dbefd56536081136280689b72e
Siedfried0988 <a0910268699@gmail.com>
2021-06-03 01:11:24 +0800

    Updates

---
+++ precache-manifest.8596ef36bbfebef3b2215441da2d18d4.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5beef26d59a59b03c3e8c7f7b3c50bb0",
+    "url": "/react/index.html"
+  },
+  {
+    "revision": "8edd3382a9a83ef8980d",
+    "url": "/react/static/css/2.014a6168.chunk.css"
+  },
+  {
+    "revision": "ad350ca937691f358233",
+    "url": "/react/static/css/main.72c4c430.chunk.css"
+  },
+  {
+    "revision": "8edd3382a9a83ef8980d",
+    "url": "/react/static/js/2.b88109e1.chunk.js"
+  },
+  {
+    "revision": "6d36de10d0af90ebf386319de2fe042d",
+    "url": "/react/static/js/2.b88109e1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ad350ca937691f358233",
+    "url": "/react/static/js/main.61e4ac1b.chunk.js"
+  },
+  {
+    "revision": "1ecd13d91122135afe2f",
+    "url": "/react/static/js/runtime-main.55f6028d.js"
+  }
+]);
